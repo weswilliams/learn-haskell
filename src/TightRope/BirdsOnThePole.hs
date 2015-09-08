280e299ac84e32dfd9bdfb875ac37203c196642d
weswilliams <wes.williamz@gmail.com>
2015-09-08 07:39:53 -0700

    more monad

---
+++ src/TightRope/BirdsOnThePole.hs
@@ -0,0 +1,17 @@
+module TightRope.BirdsOnThePole (landLeft, landRight) where
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) < 4 = Just (left, right + n)
+  | otherwise = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
